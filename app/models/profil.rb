72257150babac93b3de42d3e0b0b255a601e9c4f
Tom Gourdon <gourdontom@hotmail.fr>
2024-06-18 14:33:44 +0200

    validates faites

--- app/models/profil.rb
+++ app/models/profil.rb
@@ -4,4 +4,6 @@ class Profil < ApplicationRecord
   belongs_to :target
   belongs_to :avatar
   has_many :ingredient_recipes
+  validates :age, presence: true
+  validates :username, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients
+  validates :recipe_label, presence: true
+  validates :recipe_description, presence: true
 end
