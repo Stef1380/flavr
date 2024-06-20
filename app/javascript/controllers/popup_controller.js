ea2fcf4a189d740e63834691979f30cfe9634249
amine maachou <maachou.a13@gmail.com>
2024-06-20 16:34:22 +0200

    kk

---
+++ app/javascript/controllers/popup_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="popup"
+export default class extends Controller {
+  static targets = ["home", "flyer", "nav","bann","textInvisible"]
+  connect() {
+    console.log("d");
+  }
+  PopUp(){
+    this.flyerTarget.classList.remove("d-none")
+    this.homeTarget.classList.add("d-none")
+    this.textInvisibleTarget.style.color = "#00564A"
+    // this.navTarget.classList.add("d-none")
+    // this.bannTarget.classList.add("d-none")
+  }
+  Return(){
+    this.homeTarget.classList.remove("d-none")
+    this.flyerTarget.classList.add("d-none")
+    this.textInvisibleTarget.style.color = "white"
+  }
+}
