e0c71a55c093fad87c807cc56e2fd54a6292d53c
Stephane B <besset.stephane@gmail.com>
2024-06-21 16:46:06 +0200

    prompt v2 ok

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,14 +3,4 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
 
-  def ask_recipe(restriction, diet, preference)
-    client = OpenAI::Client.new
-    chaptgpt_response = client.chat(parameters: {
-      model: "gpt-4o",
-      messages: [{ role: "user", content: "Donne moi une recette très simple de recette de cuisine pour une personne qui mange #{diet}.
-         Qui ne peut pas manger #{restriction} et qui n'aime pas #{preference}
-         Donne moi 3 étapes pour la recette" }]
-      })
-    return chaptgpt_response["choices"][0]["message"]["content"]
-  end
 end
