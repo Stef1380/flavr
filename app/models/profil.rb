933e94441e1050f885b97e2e2622d4b277e4bd9d
Tom Gourdon <gourdontom@hotmail.fr>
2024-06-24 12:34:32 +0200

    rajout image

--- app/models/profil.rb
+++ app/models/profil.rb
@@ -2,6 +2,7 @@ class Profil < ApplicationRecord
   belongs_to :user
   belongs_to :diet
   belongs_to :target
+  belongs_to :avatar
   has_many :preferences
   has_many :ingredients, through: :preference
   has_many :restriction_profils
