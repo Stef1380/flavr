be079f33e96ac4ca32b05a6ef083617c8f226e53
Stephane B <besset.stephane@gmail.com>
2024-06-27 14:32:39 +0200

    pull à faire

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
     @recipe_ai = Recipe.recipe_ai(current_user)
   end
 
