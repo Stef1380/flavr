69c0ba189f839817b8b5cbb72bbd5321591a7054
Tom Gourdon <gourdontom@hotmail.fr>
2024-06-27 14:24:23 +0200

    photo

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,6 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
-    @recipe = Recipe.find(params[:id])
     @recipe_ai = Recipe.recipe_ai(current_user)
   end
 
